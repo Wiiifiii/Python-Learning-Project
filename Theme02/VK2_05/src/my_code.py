a598684acd30fd283ff980eb78a523ed994f6d08
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2024-09-21 14:30:50 +0300

    Implemented a number pair generator using user input. The first number increases while the second decreases, printing each pair vertically with a for-loop.

--- Theme02/VK2_05/src/my_code.py
+++ Theme02/VK2_05/src/my_code.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """
-
-KT5
+#KT 5
 
 Kysy käyttäjältä kaksi lukua. Ensimmäisessä luvussa annetaan alkuarvo  lukuparille ja toisessa montako kertaa lukuparia kasvatetaan/vähennetään.
-
 
 Tulosta lukuparit allekkain for-silmukkaa käyttäen. Vasen luku siis kasvaa ja oikea vähenee yhden verran kullakin rivillä.
 
@@ -17,10 +15,23 @@ Jos käyttäjä antaa luvut 10 ja 5, tulostus näyttää seuraavalta:
 14,6
 15,5
 
-
-
 Eli tulostuvan lukuparin ensimmäinen arvo kasvaa ja toinen vähenee lähtien liikkeelle luvusta 10 toistuen 5 kertaa
 
+#TASK 5
+Ask the user for two numbers. The first number provides the initial value for a pair of numbers, 
+and the second number determines how many times the number pair will be incremented/decremented.
+
+Print the number pairs vertically using a for-loop. The left number will increase, and the right number will decrease by one on each line.
+
+If the user inputs the numbers 10 and 5, the output should look like this:
+10,10
+11,9
+12,8
+13,7
+14,6
+15,5
+
+In other words, the first value in the printed number pair increases, and the second decreases, starting from the number 10 and repeating 5 times.
 """
 
 # Ask the user for the starting number and how many times the pair should be incremented/decremented
