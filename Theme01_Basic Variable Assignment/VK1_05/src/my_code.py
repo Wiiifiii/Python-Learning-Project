2bba5ca2d31574bd3051c178109f71b48c0df95b
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2024-09-21 14:06:38 +0300

    Added circle calculations for circumference and area based on user input for diameter.

--- Theme01_Basic Variable Assignment/VK1_05/src/my_code.py
+++ Theme01_Basic Variable Assignment/VK1_05/src/my_code.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
 
 """
-
-KT5
-
+#KT 5
 Esittele muuttuja pii, jolle annat alkuarvoksi piin likiarvon 6 desimaalin tarkkuudella.
 Lue käyttäjältä ympyrän halkaisija ja tulosta ympyrän piiri ja pinta-ala kahden desimaalin tarkkuudella
 
@@ -12,6 +9,15 @@ Malli ohessa:
 Anna ympyrän halkaisija: 12
 Piiri on 37.70
 Pinta-ala on 113.10
+
+#TASK 5
+Declare a variable pii, and assign it the approximate value of pi with an accuracy of 6 decimal places. 
+Ask the user for the diameter of a circle, then calculate and print the circumference and area of the circle with two decimal places.
+
+Example:
+Enter the diameter of the circle: 12
+The circumference is 37.70
+The area is 113.10
 
 """
 
