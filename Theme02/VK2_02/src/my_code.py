6ec9579454f7e92311bfc24ca7c6d21fcc75dfc6
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2024-09-21 14:21:28 +0300

    Added a program that evaluates a grade entered by the user and provides feedback based on the grade using if-elif-else structure.

--- Theme02/VK2_02/src/my_code.py
+++ Theme02/VK2_02/src/my_code.py
@@ -1,22 +1,27 @@
-# -*- coding: utf-8 -*-
 
 """
-KT2
-Lue käyttäjältä koenumero 4-10 väliltä. Jos käyttäjä syötti arvosanan väärin (ei väliltä 4-10) , niin tulosta "Et antanut arvosanaa annetulta väliltä". Muussa tapauksessa tulosta:
+#KT 2
+Lue käyttäjältä koenumero 4-10 väliltä. Jos käyttäjä syötti arvosanan väärin (ei väliltä 4-10) , 
+niin tulosta "Et antanut arvosanaa annetulta väliltä". Muussa tapauksessa tulosta:
+Hyvä (jos arvosana oli 9 tai 10)
+Kelpo (jos 7 - 8)
+Tyydyttävä (jos 5 - 6)
+Heikko (jos 4)
+Toteuta ohjelma if-elif-else -rakenteella.
+Ole tarkka, että tulostat juuri sen tekstin, jota pyydettiin.
 
- Hyvä (jos arvosana oli 9 tai 10)
+#TASK 2
+Read a grade from the user between 4 and 10.
+If the user enters an invalid grade (not between 4-10), print:
+"You did not give a grade from the given range".
+Otherwise, print:
+"Excellent" (if the grade is 9 or 10)
+"Good" (if the grade is between 7 and 8)
+"Satisfactory" (if the grade is between 5 and 6)
+"Weak" (if the grade is 4)
+Implement the program using if-elif-else structure.
+Ensure that the text output matches exactly as required.
 
- Kelpo (jos 7 - 8)
-
- Tyydyttävä (jos 5 - 6)
-
- Heikko (jos 4)
-
- Toteuta ohjelma if-elif-else -rakenteella.
-
- Ole tarkka, että tulostat juuri sen tekstin, jota pyydettiin.
-
- 
 """
 # Read the exam score from the user
 koenumero = int(input("Anna koenumero (4-10 väliltä): "))
