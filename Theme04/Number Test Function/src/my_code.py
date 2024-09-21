28566d56ff2ddc10e838416919b38e0bbd2b7873
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2024-09-21 17:48:34 +0300

    Added Number Test Function: Program checks if the entered number is positive, zero, or negative and returns corresponding message.

--- Theme04/Number Test Function/src/my_code.py
+++ Theme04/Number Test Function/src/my_code.py
@@ -1,27 +1,31 @@
 """
-KT1
-
+#KT 1
 
 Tee ohjelma, jossa kysytään KysyJaTestaaLuku() nimisessä funktiossa  käyttäjältä kokonaisluku. 
-
 Funktio palauttaa kokonaislukuarvon seuraavasti:
-
- 
-
 1, jos syötetty luku on positiivinen.
 0, jos syötetty luku on nolla.
-
 -1, jos syötetty luku on negatiivinen. 
-
- 
-
 Tulosta näiden paluuarvojen perusteella pääohjelmassa ilmoitus ruudulle.
-
 
 ”Luku oli positiivinen”, jos paluuarvo oli 1.
 ”Luku oli nolla”, jos paluuarvo oli 0
 ”Luku oli negatiivinen”, jos paluuarvo oli -1.
 
+#TASk 1
+Create a program where the function KysyJaTestaaLuku() asks the user for an integer.
+
+The function returns an integer value as follows:
+
+    1, if the entered number is positive.
+    0, if the entered number is zero.
+    -1, if the entered number is negative.
+
+Based on the return values, print the following messages in the main program:
+
+    "The number was positive" if the return value is 1.
+    "The number was zero" if the return value is 0.
+    "The number was negative" if the return value is -1.
 """
 
 # Define the KysyJaTestaaLuku function
