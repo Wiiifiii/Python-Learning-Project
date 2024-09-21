ed3592f55415a1fa7fd0887f42d3ba524ff7944a
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2024-09-21 13:59:45 +0300

    Added a program that reads two numbers from the user, calculates their sum, difference, and product, and displays the results in a consistent, aligned format.

--- Theme01_Basic Variable Assignment/VK1_03/src/my_code.py
+++ Theme01_Basic Variable Assignment/VK1_03/src/my_code.py
@@ -2,7 +2,8 @@
 """
 #KT 3
 Lue käyttäjältä arvot kahteen lukumuuttujaan. Nimeä muuttujat nimillä eka ja toka sekä alusta ne arvolla nolla.
-Tulosta näiden muuttujien summa, erotus ja tulo alla olevalla tavalla (HUOM! Laskutoimituksen tulostus alkaa kaikissa samasta kohdasta!)
+Tulosta näiden muuttujien summa, erotus ja tulo alla olevalla tavalla 
+(HUOM! Laskutoimituksen tulostus alkaa kaikissa samasta kohdasta!)
 Anna eka luku: 10
 Anna toka luku: 5
 Summa :        10 + 5 = 15
@@ -10,6 +11,16 @@ Erotus :       10 - 5 = 5
 Tulo :         10 * 5 = 50
 
 #TASK 3
+
+Read values from the user into two number variables. Name the variables 'first' and 'second' and initialize them to zero.
+Print the sum, difference, and product of these variables in the format shown below 
+(NOTE! The arithmetic operation output starts at the same position for all operations!)
+Example:
+Enter the first number: 10
+Enter the second number: 5
+Sum :         10 + 5 = 15
+Difference :  10 - 5 = 5
+Product :     10 * 5 = 50
 
 """
 
